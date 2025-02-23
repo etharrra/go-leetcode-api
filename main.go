e41884d55793f2affe047d39ef3da8dd67689990
etharrra <iterdare.2977@gmail.com>
2025-02-23 23:42:15 +0630

    Add: rate limiter

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"time"
+
 	"github.com/etharrra/go-leetcode-api/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.IP() == "127.0.0.1"
+		},
+		Max:        1,
+		Expiration: 30 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.SendStatus(fiber.StatusTooManyRequests)
+		},
+	}))
 
 	router.SetupRoutes(app)
 
